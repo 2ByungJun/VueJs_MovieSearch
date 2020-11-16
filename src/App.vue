6f99610af9df4b4eb5556a34bafb69dd8e987dba
2ByungJun <ieed0205@naver.com>
2020-11-17 08:09:25 +0900

    #1 vue_movie-search

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-container>
+      <!-- 검색바 -->
+      <SearchBar />
+      <!-- 영화 목록 -->
+      <MovieList />
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+// @ : 절대경로(Vue CLI)
+import SearchBar from '@/components/SearchBar'; 
+import MovieList from '@/components/MovieList'; 
+
+export default {
+  components: {
+    SearchBar,
+    MovieList
+  }
+};
+</script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+      <v-text-field
+        v-model="title"
+        outlined
+        @keypress.enter = "searchMovies" >
+        <template v-slot:prepend-inner>
+            <v-icon>search</v-icon>
+        </template>
+        <template v-slot:append>
+            <v-progress-circular
+                v-if="loading"
+                size="24"
+                color="primary"
+                indeterminate />
+        </template>
+      </v-text-field>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data(){
+        return {
+            title: '',
+            loading: false
+        }
+    },
+    methods: {
+        searchMovies() {
+            console.log("searchMovies")
+            axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=e77f1431&s=${this.title}`)
+                .then(res => {
+                    console.log(res)
+                })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
